fe3c147089ce728740cdfc5adddc1a8a98f0aacb
mangaln8 <64192305+mangaln8@users.noreply.github.com>
2020-04-23 14:22:36 +0530

    Update movie-details.php

--- movie-details.php
+++ movie-details.php
@@ -22,56 +22,50 @@ $json = json_decode(file_get_contents('http://www.omdbapi.com/?i=tt'.$a['movie']
 
 //$post–>post_category = $json["Category"];
 
-$str = "<div >
-	
-		<center><img src=$json[Poster] height='102' width='202'></center><br>
-	
-	<div class=row>
-		<div class=col-md-6>
-			Movie Title : $json[Title]  
-		</div>
-	</div>
-	<div class=row>
-		<div class=col-md-6>
-			Movie released on - $json[Released]
-		</div>
-	</div>
-	<div class=row>
-		<div class=col-md-6>
-			Genre - $json[Genre]
-		</div>
-	</div>
-	<div class=row>
-		<div class=col-md-6>
-			Director - $json[Director]
-		</div>
-	</div>
-	<div class=row>
-		<div class=col-md-6>
-		Actors - $json[Actors]
-		</div>
-	</div>
-	<div class=row>
-		<div class=col-md-6>
-			Plot - $json[Plot]
-		</div>
-	</div>
-</div>";
 
-if(count($json) > 2)
-    return $str;
-else
+	if(count($json) > 2)
+	{
+		$str = "<div >
+
+			<center><img src=$json[Poster] height='102' width='202'></center><br>
+
+		<div class=row>
+			<div class=col-md-6>
+				Movie Title : $json[Title]  
+			</div>
+		</div>
+		<div class=row>
+			<div class=col-md-6>
+				Movie released on - $json[Released]
+			</div>
+		</div>
+		<div class=row>
+			<div class=col-md-6>
+				Genre - $json[Genre]
+			</div>
+		</div>
+		<div class=row>
+			<div class=col-md-6>
+				Director - $json[Director]
+			</div>
+		</div>
+		<div class=row>
+			<div class=col-md-6>
+			Actors - $json[Actors]
+			</div>
+		</div>
+		<div class=row>
+			<div class=col-md-6>
+				Plot - $json[Plot]
+			</div>
+		</div>
+		
+		</div>";
+
+	    return $str;
+	}
+	else
 	return "<h5>No Movie with that IMDB ID</h5>";
 
-
-       // $curl=curl_init();
-       // curl_setopt($curl, CURLOPT_URL,'http://www.omdbapi.com/?i=tt1540133&apikey=d59eb885');
-       // curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-       // $jso = curl_exec($curl);
-       // curl_close($curl);
-        
-        //$json = json_decode($jso, true); //This will convert it to an array
-      //  $movie_title = $json['Title'];
-       // return $movie_title;
 }
 add_shortcode('Movie', 'movie_detail');
